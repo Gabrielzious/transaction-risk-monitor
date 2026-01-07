9095f28c8592509b2371be084c0c2c03d4426990
Gabriel Henrique Furlan <gabrielzious@Mac.home>
2026-01-07 16:03:12 -0300

    first commit

---
+++ src/main/java/furlan/project/transaction/TransactionRiskMonitorApplication.java
@@ -0,0 +1,13 @@
+package furlan.project.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRiskMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionRiskMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/furlan/project/transaction/TransactionRiskMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package furlan.project.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionRiskMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
